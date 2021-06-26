69fbed1e973b4557b8b776b890f75be63dc1e33c
Adilson Perinei <adilson@perinei.com>
2021-06-26 13:29:18 -0400

    config

--- js/aws/config.js
+++ js/aws/config.js
@@ -2,7 +2,7 @@ var awsConfig = {
     regionName : 'us-east-1',
     userPoolId : 'us-east-1_xj6SEtAyE',
     clientId : '30r03gkfc6s448eqgrautmdmj9',
-    identityPoolId : 'us-east-1:xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
+    identityPoolId : 'us-east-1:c59f5a04-0d63-428c-b24a-612980714b62',
     samlIdpArn: 'arn:aws:iam::<aws-account-no>:saml-provider/cognito-s3-saml',
     relayingPartyId: 'cognito-s3-internal',
     adfsUrl: 'https://<adfs-server-ip>/adfs/ls/IdpInitiatedSignOn.aspx',
